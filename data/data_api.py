fb560488431d7f4be7e9018ffe0d33c0792dc861
am1nix <am1nix@mail.ru>
2025-11-12 06:03:50 +0400

    feat: updated data generator with unique_id

--- data/data_api.py
+++ data/data_api.py
@@ -1,19 +1,33 @@
+import uuid
+from typing import Optional
 from api.requests.models import EntityRequest, AdditionRequest
 
+
 def create_entity_payload(
-        title="Default Test Title",
-        verified=True,
-        info="Default additional info",
-        number=100,
-        important_numbers=[1, 2, 3]
+        title: Optional[str] = None,
+        verified: bool = True,
+        info: Optional[str] = None,
+        number: int = 100,
+        important_numbers: list[int] = None
 ) -> EntityRequest:
-    """Генерирует объект EntityRequest с тестовыми данными."""
+    """
+    Генерирует объект EntityRequest с тестовыми данными.
+    Если title или info не переданы, генерирует уникальные значения.
+    """
+    unique_id = uuid.uuid4()
+
+    final_title = title if title is not None else f"Test Title {unique_id}"
+
+    final_info = info if info is not None else f"Additional info {unique_id}"
+
+    final_important_numbers = important_numbers if important_numbers is not None else [1, 2, 3]
+
     return EntityRequest(
         addition=AdditionRequest(
-            additional_info=info,
+            additional_info=final_info,
             additional_number=number
         ),
-        important_numbers=important_numbers,
-        title=title,
+        important_numbers=final_important_numbers,
+        title=final_title,
         verified=verified
     )
